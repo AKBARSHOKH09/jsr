38efe38f4dec767ffff1c267a1ee9f0c3b0a014a
AKBARSHOKH09 <muhammadjonovakbar073@gmail.com>
2024-11-25 17:31:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let number = 10
+
+const obj = {
+    id: 1,
+    name: 'Akbar',
+    age: 15,
+    surname: 'Muhammadjonov',
+    shamsiddin: {
+        boyi: 'norm',
+        yoshi: 15,
+        sochiTuri: 'kaskat soch',
+        ovozi: `vo'r`,
+        etibor: 'qizlani babniki',
+        eskiJonkasi: 'asqarri jokasi',
+
+    }
+
+}
+
+obj.oquvchi = 'Shamsiddinga'
+obj.siroch.tili = 'sokish '
+delete obj.shamsiddin.etibori
+console.log(obj);
